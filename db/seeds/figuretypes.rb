d3103d99f4cc5bca746a7fd6e26f6df76aa30cdd
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-01-02 14:07:52 -0500

    added in figure types and created multiple seed files

---
+++ db/seeds/figuretypes.rb
@@ -0,0 +1,20 @@
+figuretypes_data = FigureType.create([
+  {
+    figure_type: 'Nendroid',
+    figure_img: 'https://i.ibb.co/T4cCnCV/Screen-Shot-2023-11-06-at-12-02-52-PM.png'
+  }, 
+  {
+    figure_type: 'Funko',
+    figure_img: 'https://i.ibb.co/H2Fw9m4/Screen-Shot-2023-11-06-at-12-06-55-PM.png'
+  },
+  {
+    figure_type: 'PVC/Prize',
+    figure_img: 'https://i.ibb.co/y6Ybg7m/Screen-Shot-2023-11-06-at-12-12-42-PM.png'
+  },
+  {
+    figure_type: 'Scale Figure',
+    figure_img: 'https://i.ibb.co/wsmdh7Q/Screen-Shot-2023-11-06-at-12-18-17-PM.png'
+  }
+])
+
+puts 'we have our figure types!'
