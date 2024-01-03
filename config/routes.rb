5f751ce01ef152516abf40a9b32c243a6359b242
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-01-03 12:16:09 -0500

    starting point for figures api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :figures
   namespace :api do 
     namespace :v1 do 
       resources :animes, only: [:index, :show]
---
+++ db/migrate/20240103165228_create_figures.rb
@@ -0,0 +1,20 @@
+class CreateFigures < ActiveRecord::Migration[6.0]
+  def change
+    create_table :figures do |t|
+      t.references :figuretype, foreign_key: true 
+      t.references :anime, foreign_key: true 
+      t.string :figure_name
+      t.string :figure_second_name
+      t.string :rarity
+      t.string :price
+      t.string :rating
+      t.string :img_link
+      t.string :character
+      t.string :size
+      t.string :material
+      t.string :manufacturer
+      t.boolean :safe_for_work
+      t.timestamps
+    end
+  end
+end
